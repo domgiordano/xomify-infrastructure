155a481ebdd186fade94511c25dad790c0535c55
Dominick Giordano <dominickgiordano@Dominicks-MacBook-Pro.local>
2026-01-31 08:21:41 -0500

    correction

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -31,7 +31,7 @@ resource "aws_cloudwatch_log_group" "groups_db_log_group" {
 }
 
 resource "aws_cloudwatch_log_group" "group_members_db_log_group" {
-    name = aws_dynamodb_table.groups.id
+    name = aws_dynamodb_table.group_members.id
     retention_in_days = 14
     tags = merge(local.standard_tags, tomap({ "name"= "${var.app_name}-group-members"}))
 }
