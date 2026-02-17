45b4267ace2c9c8dffbc3423b701fb979454e31a
Dominick Giordano <dominickgiordano@Dominicks-MacBook-Pro.local>
2026-02-17 11:26:48 -0500

    WAF association sep

--- terraform/waf.tf
+++ terraform/waf.tf
@@ -15,9 +15,14 @@ module "waf_cloudfront" {
 module "waf_api_gateway" {
   source = "git::https://github.com/domgiordano/waf.git?ref=v1.0.0"
 
-  app_name     = "${var.app_name}-api-gateway"
-  scope        = "REGIONAL"
-  rate_limit   = 2000
+  app_name   = "${var.app_name}-api-gateway"
+  scope      = "REGIONAL"
+  rate_limit = 2000
+  tags       = local.standard_tags
+}
+
+# Associate WAF with API Gateway stage (separate to avoid unknown count)
+resource "aws_wafv2_web_acl_association" "api_gateway" {
+  web_acl_arn  = module.waf_api_gateway.web_acl_arn
   resource_arn = module.api.stage_arn
-  tags         = local.standard_tags
 }
