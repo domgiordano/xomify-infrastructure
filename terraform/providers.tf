74068d1998f877c122c8a8586b2eef2782da4143
JarvisXomware <jarvis@xomware.com>
2026-02-21 14:28:24 -0500

    feat: migrate from static AWS creds to OIDC auth
    Replace static access_key/secret_key variables with OIDC-based authentication
    between Terraform Cloud and AWS. This eliminates long-lived IAM credentials.
    
    - Remove access_key and secret_key variables
    - Add aws_role_arn variable for OIDC assume role
    - Update AWS provider to use assume_role
    - Add oidc.tf with setup documentation
    - Update README with OIDC auth section
    
    Closes #33

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -6,8 +6,11 @@ terraform {
     }
   }
 }
+
 provider "aws" {
-  access_key = var.access_key
-  secret_key = var.secret_key
-  region     = var.aws_region
+  region = var.aws_region
+
+  assume_role {
+    role_arn = var.aws_role_arn
+  }
 }
