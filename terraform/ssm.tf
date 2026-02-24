52a0e95e140d6edc1c29301b6c7b628bf3b540a3
JarvisXomware <jarvis@xomware.com>
2026-02-24 14:43:51 -0500

    fix: remove SSM params referencing deleted access_key/secret_key vars
    The OIDC migration removed the static AWS credential variables but
    left SSM parameter resources that referenced them, breaking terraform validate.

--- terraform/ssm.tf
+++ terraform/ssm.tf
@@ -1,19 +1,3 @@
-# AWS
-resource "aws_ssm_parameter" "access_key" {
-  name        = "/${var.app_name}/aws/ACCESS_KEY"
-  description = "AWS Access Key"
-  type        = "SecureString"
-  value       = var.access_key
-  tags        = merge(local.standard_tags, tomap({ "name" = "${var.app_name}-aws-access-key" }))
-}
-resource "aws_ssm_parameter" "secret_key" {
-  name        = "/${var.app_name}/aws/SECRET_KEY"
-  description = "AWS Secret Key"
-  type        = "SecureString"
-  value       = var.secret_key
-  tags        = merge(local.standard_tags, tomap({ "name" = "${var.app_name}-aws-secret-key" }))
-}
-
 # SPOTIFY
 resource "aws_ssm_parameter" "client_id" {
   name        = "/${var.app_name}/spotify/CLIENT_ID"
